ccb616b284c3c6dd63f91057e08943916f37c614
shad <kotabe10@gmail.com>
2024-12-14 10:54:45 +0900

    WIP Implement View Files(admin)

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -10,69 +10,104 @@
 @endsection
 
 @section('content')
-<div class="contact-form__content">
+<div class="search-form__content">
   <div class="section__title">
     <h2>Admin</h2>
   </div>
-    <div class="admin-table">
-      <table class="confirm-table__inner">
-        <tr class="confirm-table__row">
-          <th class="admin-table__header--name">お名前</th>
-          <th class="admin-table__header--gender">性別</th>
-          <th class="admin-table__header--email">メールアドレス</th>
-          <th class="admin-table__header--category">お問い合わせの種類</th>
-          <th class="admin-table__header--blank"></th>
-        </tr>
-        <tr class="admin-table__row">
-          <td class="admin-table__content--name">山田　太郎</td>
-          <td class="admin-table__content--gender">男性</td>
-          <td class="admin-table__content--email">test@example.com</td>
-          <td class="admin-table__content--category">商品の交換について</td>
-          <td class="admin-table__content--button">
-            <button id="openModalBtn" class="admin-table__button-submit">詳細</button>
-          </td>
-        </tr>
-      </table>
+  <form class="search-form" action="/admin/search">
+    @csrf
+    <div class="search-form__item">
+      <input class="search-form__input-text" type="text" placeholder="名前やメールアドレスをいれてください" />
+      <select class="search-form__select--gender">
+        <option value="">性別</option>
+      </select>
+      <select class="search-form__select--category">
+        <option value="">お問い合わせの種類</option>
+      </select>
+      <input type="date" class="search-form__input-date">
+      <div class="search-form__button">
+        <button class="search-form__button-submit" type="submit">検索</button>
+      </div>
+      <div class="search-form__button">
+        <button class="search-form__button-reset" type="reset">リセット</button>
+      </div>
     </div>
-  <!-- モーダル -->
-  <div id="myModal" class="modal">
-    <div class="modal-content">
-      <!-- 右上の閉じるボタン -->
-      <button class="close-btn-top" id="closeModalTopBtn">&times;</button>
-      <div class="modal-table">
-        <table class="modal-table__inner">
-          <tr class="modal-table__row">
-            <th class="modal-table__header">お名前</th>
-            <td class="modal-table__content">山田　太郎</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">性別</th>
-            <td class="modal-table__content">男性</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">メールアドレス</th>
-            <td class="modal-table__content">test@example.com</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">電話番号</th>
-            <td class="modal-table__content">08012345678</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">住所</th>
-            <td class="modal-table__content">東京都渋谷区千駄ヶ谷1-2-3</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">建物名</th>
-            <td class="modal-table__content">千駄ヶ谷マンション</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">お問い合わせの種類</th>
-            <td class="modal-table__content">商品の交換について</td>
-          </tr>
-          <tr class="modal-table__row">
-            <th class="modal-table__header">お問い合わせ内容</th>
-            <td class="modal-table__content">
-              <textarea  id="autoResizeTextarea" class="text_area" readonly >
+  </form>
+  </div>
+
+  <div class="table-controls">
+    <form class="export-form" action="/data/export" method="POST">
+      @csrf
+      <div class="table-export">
+        <input type="hidden" name="data" id="hiddenData">
+        <button class="table-export__button-submit" type="submit">エクスポート
+        </button>
+      </div>
+    </form>
+    <div class="table-pagenation">
+      Pagenation
+    </div>
+  </div>
+  <div class="admin-table">
+    <table class="admin-table__inner">
+      <tbody>
+      <tr class="admin-table__row">
+        <th class="admin-table__header--name">お名前</th>
+        <th class="admin-table__header--gender">性別</th>
+        <th class="admin-table__header--email">メールアドレス</th>
+        <th class="admin-table__header--category">お問い合わせの種類</th>
+        <th class="admin-table__header--blank"></th>
+      </tr>
+      <tr class="admin-table__row">
+        <td class="admin-table__content--name">山田　太郎</td>
+        <td class="admin-table__content--gender">男性</td>
+        <td class="admin-table__content--email">test@example.com</td>
+        <td class="admin-table__content--category">商品の交換について</td>
+        <td class="admin-table__button">
+          <button id="openModalBtn" class="admin-table__button-submit">詳細</button>
+        </td>
+      </tr>
+    </tbody>
+    </table>
+  </div>
+<!-- モーダル -->
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <button class="close-btn-top" id="closeModalTopBtn">&times;</button>
+    <div class="modal-table">
+      <table class="modal-table__inner">
+        <tr class="modal-table__row">
+          <th class="modal-table__header">お名前</th>
+          <td class="modal-table__content">山田　太郎</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">性別</th>
+          <td class="modal-table__content">男性</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">メールアドレス</th>
+          <td class="modal-table__content">test@example.com</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">電話番号</th>
+          <td class="modal-table__content">08012345678</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">住所</th>
+          <td class="modal-table__content">東京都渋谷区千駄ヶ谷1-2-3</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">建物名</th>
+          <td class="modal-table__content">千駄ヶ谷マンション</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">お問い合わせの種類</th>
+          <td class="modal-table__content">商品の交換について</td>
+        </tr>
+        <tr class="modal-table__row">
+          <th class="modal-table__header">お問い合わせ内容</th>
+          <td class="modal-table__content">
+            <textarea  id="autoResizeTextarea" class="text_area" readonly >
 このテキストエリアは readonly 属性が設定されています。
 ユーザーはこの内容を編集することができませんが、コピーすることは可能です。
 ユーザーはこの内容を編集することができませんが、コピーすることは可能です。
@@ -84,7 +119,7 @@
       <button class="close-btn" id="closeModalBtn">削除</button>
     </div>
   </div>
-    <script>
+  <script>
     // JavaScriptでモーダルの開閉を制御
     const openModalBtn = document.getElementById('openModalBtn');
     const closeModalBtn = document.getElementById('closeModalBtn');
